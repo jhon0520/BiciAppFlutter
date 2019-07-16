9b163cfcf6fe84439f5a9c2678f3b05913ec2137
jhon0520 <jhonedman@gmail.com>
2019-07-16 17:27:43 -0500

    Pantalla de login

---
+++ lib/src/pages/login_page.dart
@@ -0,0 +1,149 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  LoginPage({Key key}) : super(key: key);
+
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool dayMode = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+            child: Text(
+          'RODAPP',
+          textAlign: TextAlign.center,
+          style: TextStyle(),
+        )),
+        actions: <Widget>[
+          _switchButton(),
+        ],
+      ),
+      body: Stack(
+        children: <Widget>[
+          _colorFondo(),
+          _imagenFondo(),
+          _loginInputs(),
+        ],
+      ),
+    );
+  }
+
+  Widget _colorFondo() {
+    if (dayMode) {
+      return Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.lightBlue[50],
+      );
+    } else if (!dayMode) {
+      return Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.black,
+      );
+    }
+  }
+
+  Widget _switchButton() {
+    return Switch(
+      value: dayMode,
+      onChanged: (value) {
+        setState(() {
+          dayMode = value;
+        });
+      },
+      activeThumbImage: Image.asset('assets/icons/sun.png').image,
+      activeColor: Colors.white,
+      activeTrackColor: Colors.yellow,
+      inactiveThumbImage: Image.asset('assets/icons/moon.png').image,
+      inactiveTrackColor: Colors.black,
+    );
+  }
+
+  Widget _imagenFondo() {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      child: Image(
+        image: AssetImage('assets/icons/fondo.png'),
+        fit: BoxFit.fill,
+      ),
+    );
+  }
+
+  Widget _loginInputs() {
+    final estiloTitulo = TextStyle(
+        color: Colors.black, fontWeight: FontWeight.w600, fontSize: 40.0);
+
+    return SafeArea(
+      child: Column(
+        children: <Widget>[
+          SizedBox(height: 50.0),
+          Text(
+            'Iniciar Sesión',
+            style: estiloTitulo,
+          ),
+          SizedBox(height: 50.0),
+          TextField(
+            obscureText: true,
+            decoration: InputDecoration(
+                icon: Icon(
+                  Icons.account_circle,
+                  color: Colors.black,
+                  size: 50.0,
+                ),
+                labelText: 'Usuario',
+                labelStyle: TextStyle(color: Colors.black, fontSize: 25.0),
+                helperStyle: TextStyle(color: Colors.white24),
+                contentPadding: EdgeInsets.symmetric(horizontal: 20.0)),
+          ),
+          SizedBox(
+            height: 30.0,
+          ),
+          //Contraseña
+          TextField(
+            obscureText: true,
+            decoration: InputDecoration(
+                icon: Icon(
+                  Icons.lock_outline,
+                  color: Colors.black,
+                  size: 50.0,
+                ),
+                labelText: 'Contraseña',
+                labelStyle: TextStyle(color: Colors.black, fontSize: 25.0),
+                helperStyle: TextStyle(color: Colors.white24),
+                contentPadding: EdgeInsets.symmetric(horizontal: 20.0)),
+          ),
+          // Login Button.
+          SizedBox(height: 30),
+          RaisedButton(
+            onPressed: () {},
+            textColor: Colors.white,
+            padding: EdgeInsets.all(0.0),
+            child: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: <Color>[
+                    Color(0xFF42A5F5),                   
+                    Color(0xFF1976D2), 
+                    Color(0xFF0D47A1),                   
+                  ],
+                ),
+                borderRadius: BorderRadius.all(Radius.circular(5.0))
+              ),
+              // Padding del boton.
+              padding: EdgeInsets.symmetric(horizontal: 50.0, vertical: 15.0),
+              child:
+                  const Text('Ingresar', style: TextStyle(fontSize: 20)),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
