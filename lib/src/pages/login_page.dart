036b878cb600d9288070b1f727d6a7722ea40812
jhon0520 <jhonedman@gmail.com>
2019-07-17 17:52:51 -0500

    Actualizando LoginPage

--- lib/src/pages/login_page.dart
+++ lib/src/pages/login_page.dart
@@ -1,5 +1,3 @@
-import 'dart:collection';
-
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -15,42 +13,48 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(
-            child: Text(
-          'RODAPP',
-          textAlign: TextAlign.center,
-          style: TextStyle(),
-        )),
+        title: Text('RODAPP'),
+        centerTitle: true,
         actions: <Widget>[
           _switchButton(),
         ],
       ),
       body: Stack(
+        fit: StackFit.expand,
         children: <Widget>[
-          _colorFondo(),
-          _imagenFondo(),
-          _loginInputs(context),
-          _olvidarContrasenayRegistro(),
-          _redesSociales(),
+          _fondo(),
+          _loginInputs(),
         ],
       ),
     );
   }
 
-  Widget _colorFondo() {
+  Widget _fondo() {
+    Container containerMode = Container();
+
     if (dayMode) {
-      return Container(
+      containerMode = Container(
         width: double.infinity,
         height: double.infinity,
         color: Colors.lightBlue[50],
+        child: Image(
+          image: AssetImage('assets/img/fondo.png'),
+          fit: BoxFit.fill,
+        ),
       );
     } else if (!dayMode) {
-      return Container(
+      containerMode = Container(
         width: double.infinity,
         height: double.infinity,
         color: Colors.black,
+        child: Image(
+          image: AssetImage('assets/img/fondo.png'),
+          fit: BoxFit.fill,
+        ),
       );
     }
+
+    return containerMode;
   }
 
   Widget _switchButton() {
@@ -69,91 +73,160 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 
-  Widget _imagenFondo() {
-    return Container(
-      width: double.infinity,
-      height: double.infinity,
-      child: Image(
-        image: AssetImage('assets/img/fondo.png'),
-        fit: BoxFit.fill,
-      ),
-    );
-  }
+  Widget _loginInputs() {
+    final size = MediaQuery.of(context).size;
 
-  Widget _loginInputs(BuildContext context) {
     final estiloTitulo = TextStyle(
-        color: Colors.black, fontWeight: FontWeight.w600, fontSize: 40.0);
+        color: Colors.black, fontWeight: FontWeight.w600, fontSize: 30.0);
 
-    return SafeArea(
+    return SingleChildScrollView(
       child: Column(
         children: <Widget>[
-          SizedBox(height: 50.0),
-          Text(
-            'Iniciar Sesión',
-            style: estiloTitulo,
+          SafeArea(
+            child: Container(),
           ),
-          SizedBox(height: 50.0),
-          TextField(
-            obscureText: true,
-            decoration: InputDecoration(
-                icon: Icon(
-                  Icons.account_circle,
-                  color: Colors.black,
-                  size: 50.0,
+          Container(
+            width: size.width * 0.90,
+            margin: EdgeInsets.symmetric(vertical: 30.0),
+            padding: EdgeInsets.symmetric(vertical: 50.0),
+            color: Colors.transparent,
+            child: Column(
+              children: <Widget>[
+                Text(
+                  'Iniciar Sesión',
+                  style: estiloTitulo,
                 ),
-                labelText: 'Usuario',
-                labelStyle: TextStyle(color: Colors.black, fontSize: 25.0),
-                helperStyle: TextStyle(color: Colors.white24),
-                contentPadding: EdgeInsets.symmetric(horizontal: 20.0)),
-          ),
-          SizedBox(
-            height: 30.0,
-          ),
-          //Contraseña
-          TextField(
-            obscureText: true,
-            decoration: InputDecoration(
-                icon: Icon(
-                  Icons.lock_outline,
-                  color: Colors.black,
-                  size: 50.0,
-                ),
-                labelText: 'Contraseña',
-                labelStyle: TextStyle(color: Colors.black, fontSize: 25.0),
-                helperStyle: TextStyle(color: Colors.white24),
-                contentPadding: EdgeInsets.symmetric(horizontal: 20.0)),
-          ),
-          // Login Button.
-          SizedBox(height: 30),
-          Builder(
-            builder: (context) => RaisedButton(
-              onPressed: () {
-                final snackBar = SnackBar(
-                  content: Text('Yay! A SnackBar!'),
-                );
-                Scaffold.of(context).showSnackBar(snackBar);
-              },
-              textColor: Colors.white,
-              padding: EdgeInsets.all(0.0),
-              child: Container(
-                decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: <Color>[
-                        Color(0xFF42A5F5),
-                        Color(0xFF1976D2),
-                        Color.fromRGBO(34, 139, 134, 100.0),
-                        //Color(0xFF0D47A1),
-                      ],
-                    ),
-                    borderRadius: BorderRadius.all(Radius.circular(5.0))),
-                // Padding del boton.
-                padding: EdgeInsets.symmetric(horizontal: 50.0, vertical: 15.0),
-                child: Text('Ingresar', style: TextStyle(fontSize: 20)),
-              ),
+                SizedBox(height: 20.0),
+                _crearUsuario(),
+                SizedBox(height: 20.0),
+                _crearContrasena(),
+                SizedBox(height: 20.0),
+                _crearBotonLogin(),
+                SizedBox(height: 20.0),
+                _crearOlvidoContrasena(),
+              ],
             ),
           ),
         ],
       ),
+    );
+  }
+
+  Widget _crearUsuario() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: TextField(
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+          icon: Icon(
+            Icons.account_circle,
+            color: Colors.black,
+            size: 50.0,
+          ),
+          hintText: 'ejemplo@correo.com',
+          labelText: 'Usuario',
+          counterText: 'Usuario',
+          labelStyle: TextStyle(color: Colors.black),
+          hintStyle: TextStyle(color: Colors.black),
+        ),
+      ),
+    );
+  }
+
+  Widget _crearContrasena() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: TextField(
+        obscureText: true,
+        decoration: InputDecoration(
+          icon: Icon(
+            Icons.lock_outline,
+            color: Colors.black,
+            size: 50.0,
+          ),
+          labelText: 'Contraseña',
+          counterText: 'Contraseña',
+          labelStyle: TextStyle(color: Colors.black),
+          hintStyle: TextStyle(color: Colors.black),
+        ),
+      ),
+    );
+  }
+
+  Widget _crearBotonLogin() {
+    return Builder(
+      builder: (context) => RaisedButton(
+        onPressed: () {
+          final snackBar = SnackBar(
+            content: Text('Yay! A SnackBar!'),
+          );
+          Scaffold.of(context).showSnackBar(snackBar);
+        },
+        textColor: Colors.white,
+        padding: EdgeInsets.all(0.0),
+        child: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: <Color>[
+                  Color(0xFF42A5F5),
+                  Color(0xFF1976D2),
+                  Color.fromRGBO(34, 139, 134, 100.0),
+                  //Color(0xFF0D47A1),
+                ],
+              ),
+              borderRadius: BorderRadius.all(Radius.circular(5.0))),
+          // Padding del boton.
+          padding: EdgeInsets.symmetric(horizontal: 50.0, vertical: 15.0),
+          child: Text('Ingresar', style: TextStyle(fontSize: 20)),
+        ),
+      ),
+    );
+  }
+
+  Widget _crearOlvidoContrasena(){
+
+    return Container(
+        child: Column(
+          children: <Widget>[
+            FlatButton(
+              color: Colors.transparent,
+              textColor: Colors.black,
+              child: Text('Olvidaste tu contraseña?'),
+              onPressed: () {
+                print('holasssssssssss');
+              },
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text('No tienes cuenta?',
+                    style: TextStyle(
+                      color: Colors.black,
+                    )),
+                FlatButton(
+                  color: Colors.transparent,
+                  textColor: Colors.black,
+                  child: Text('Registrate'),
+                  onPressed: () {
+                    print('holasssssssssss');
+                  },
+                ),
+              ],
+            ),
+            Container(
+              color: Colors.transparent,
+              width: MediaQuery.of(context).size.width / 1.5,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(child: Divider()),
+                  Text(" O "),
+                  Expanded(child: Divider()),
+                ],
+              ),
+            ),
+          ],
+        )
     );
   }
 
@@ -229,7 +302,9 @@ class _LoginPageState extends State<LoginPage> {
                     print('object Google');
                   },
                 ),
-                SizedBox(width: MediaQuery.of(context).size.width/4,),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width / 6,
+                ),
                 IconButton(
                   icon: Image.asset('assets/icons/facebook.png'),
                   iconSize: 50.0,
